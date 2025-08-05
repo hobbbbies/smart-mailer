6190ba72252ba5819493f733ba0b3f8f172816e9
hobbbbies <stefankvitanov@gmail.com>
2025-08-05 13:17:07 -0400

    node_env variable

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -4,14 +4,18 @@ const controller = require('../controllers/emailController');
 const llm = require('../services/llm');
 const multer = require('multer');
 const upload = multer();
+require('dotenv').config();
 
-// --DEVELOPMENT MODEL--
-router.post('/', llm.generateEmailContent);
-router.post('/update', llm.updateEmailContent);    
 
-// --PRODUCTION MODEL--
-// router.post('/', llm.generateEmailContent_OpenAI);
-// router.post('/update', llm.updateEmailContent_OpenAI);
+if (process.env.NODE_ENV !== 'production') {
+    // --DEVELOPMENT MODEL (Local Ollama)--
+    router.post('/', llm.generateEmailContent);
+    router.post('/update', llm.updateEmailContent);    
+} else {
+    // --PRODUCTION MODEL (OpenAI)--
+    router.post('/', llm.generateEmailContent_OpenAI);
+    router.post('/update', llm.updateEmailContent_OpenAI);
+}
 router.post('/send-gmail', upload.array('attachments'), controller.sendEmailGmailApi); // Change to upload.array for multiple extensions later
 router.post('/send-thirdParty', controller.sendEmailResendAPI);
 
